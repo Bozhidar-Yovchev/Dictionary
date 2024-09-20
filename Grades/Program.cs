2142f9ec24bbf3b9b324bbbde73fd72e5d9f7ab0
Bozhidar-Yovchev <bozhidar601@gmail.com>
2024-09-20 20:19:50 +0300

    addition

---
+++ Grades/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number of students:");
+            int n = int.Parse(Console.ReadLine());
+            var grades = new Dictionary<string, List<double>>();
+            Console.WriteLine("Enter name and grade:");
+            for (int i = 0; i < n; i++)
+            {
+                var tokens = Console.ReadLine().Split();
+                var name = tokens[0];
+                var grade = double.Parse(tokens[1]);
+
+                if (!grades.ContainsKey(name))
+                {
+                    grades[name] = new List<double>();
+                }
+                grades[name].Add(grade);
+            }
+
+            foreach (var pair in grades)
+            {
+                var name = pair.Key;
+                var studentGrade = pair.Value;
+                var average = studentGrade.Average();
+
+
+                Console.Write($"{name} -> ");
+                foreach (var grade in studentGrade)
+                {
+                    Console.Write($"{grade:f2} ");
+                    Console.WriteLine($"avg: {average:f2}");
+                }
+            }
+        }
+    }
+}
---
+++ Shop/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var shop = new Dictionary<string, Dictionary<string, double>>();
+            string line;
+
+            Console.WriteLine("Enter magazine, product, price or Revision to end the program");
+            while ((line = Console.ReadLine()).ToLower() != "Revision")
+            {
+
+                var tokens = Console.ReadLine().Split(',');
+                var magazine = tokens[0];
+                var product = tokens[1];
+                var price = double.Parse(tokens[2]);
+
+                if (!shop.ContainsKey(magazine))
+                {
+                    shop.Add(magazine, new Dictionary<string, double>());
+                }
+                shop[magazine].Add(product, price);
+            }
+
+            foreach (var item in shop)
+            {
+                var shopName = item.Key;
+                Console.WriteLine($"{shopName}:");
+                foreach (var itemshop in item.Value)
+                {
+                    var productName = itemshop.Key;
+                    var productPrice = itemshop.Value;
+                    Console.WriteLine($"Product: {productName}, Price: {productPrice}");
+                }
+            }
+        }
+    }
+}
